7fb0ff560f17b7d560052f0911ffb2aba3bf31c1
Jason Juhyun Kim <mtinet@hanmail.net>
2019-01-01 12:00:26 +0900

    Create band.py

---
+++ band.py
@@ -0,0 +1,18 @@
+import json
+import random
+
+data = json.loads(open("instruments.json").read())
+instruments = data['instruments']
+
+band = random.sample(instruments, 10)
+
+output = "The Randoms : \n"
+
+for instrument in band:
+    output += instrument + "\n"
+
+print(output)
+
+with open('theband.txt', 'w') as file:
+    file.write(output)
+    
